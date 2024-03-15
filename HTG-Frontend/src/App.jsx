45d71ca9b289caf49d133cd86f3812fc114e1127
khoaalski <kbsch28@gmail.com>
2024-03-15 16:01:07 -0700

    Added title and restyled buttons. Questions: do we want to create folders for components and styles? And should we change the background color to white so that it's more hospital-y?

--- HTG-Frontend/src/App.jsx
+++ HTG-Frontend/src/App.jsx
@@ -5,8 +5,16 @@ function App() {
 
   return (
     <>
-      <a href="/patient-info"><button>Add Patient Information</button></a>
-      <button>See Map</button>
+      <div className="title">
+        <h3>ABC Hospital</h3>
+        <h1>Bed Allocation System</h1>
+      </div>
+      <div>
+      <a href="/patient-info"><button className="addPatientButton">Add Patient</button></a>
+      </div>
+      <div>
+      <button className="seeMapButton">See Map</button>
+      </div>
     </>
   )
 }
